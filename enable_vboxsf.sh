800aa4e8c7c80b2b004acf2fc6fc41ea5c1e2727
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-13 02:19:01 +0900

    modified: enable_vboxsf.sh
    new option: -h

--- enable_vboxsf.sh
+++ enable_vboxsf.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
+function usage(){
+cat <<_EOT_
+Usage:
+   enable_vboxsf.sh [-h]
+
+Description:
+   Virtual Box のゲスト OS へ共有ディレクトリへのアクセス権を付与します。
+
+Options:
+   -h ヘルプを表示します。
+_EOT_
+exit 1
+}
+
+while getopts "h" option
+do
+   case $option in
+      h)
+         usage
+         ;;
+      \?)
+         usage
+         ;;
+   esac
+done
+
 user_name="$1"
 gpasswd -a "$user_name" vboxsf
