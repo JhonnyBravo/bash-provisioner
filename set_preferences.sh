a04261110cd7cc9be42965841d8f134842646ad9
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-14 04:21:27 +0900

    set_preferences.sh

---
+++ set_preferences.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+u_path=".config/sublime-text-2/Packages/User/"
+
+function usage(){
+cat <<_EOT_
+Usage:
+   set_preferences.sh [path] [-h]
+
+Description:
+   指定したファイルを User/Preferences.sublime-settings へコピーします。
+
+Options:
+   -h ヘルプを表示します。
+_EOT_
+exit 1
+}
+
+while getopts "h" option
+do
+   case $option in
+      h)
+         usage
+         ;;
+      \?)
+         usage
+         ;;
+   esac
+done
+
+cp "$1" ~/"${u_path}"
