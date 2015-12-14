673b76c7300b55206b6820b4187f887c4f993a45
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-15 01:10:09 +0900

    new file: new_item.sh

---
+++ new_item.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function usage(){
+cat <<_EOT_
+Usage:
+   new_item.sh [-f path [value]] [-d path] [-h]
+
+Description:
+   ファイル / ディレクトリを新規作成します。
+
+Options:
+   -f 新規ファイルを作成します。 value を省略した場合、空のファイルを作成します。
+   -d 新規ディレクトリを作成します。
+   -h ヘルプを表示します。
+_EOT_
+exit 1
+}
+
+f_flag=0
+d_flag=0
+
+while getopts "f:d:h" option
+do
+   case $option in
+      f)
+         f_flag=1
+         ;;
+      d)
+         d_flag=1
+         ;;
+      h)
+         usage
+         ;;
+      \?)
+         usage
+         ;;
+   esac
+done
+
+shift $(expr $OPTIND - 2)
+path="$1"
+
+if [ $f_flag -eq 1 ]; then
+   value="$2"
+   echo "$value" >"$path"
+elif [ $d_flag -eq 1 ]; then
+   mkdir -p "$path"
+fi
