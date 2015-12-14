91d88ed604b2df8f37b72725dfbad9f5165ca70a
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-14 15:35:59 +0900

    remove_key.sh

---
+++ remove_key.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function usage(){
+cat <<_EOT_
+Usage:
+   remove_key.sh file_path key [-h]
+
+Description:
+   JSON ファイルから key を削除します。
+
+Options:
+   -h ヘルプを表示します。
+_EOT_
+exit 1
+}
+
+while getopts "h" option
+do
+   case $option in
+      h)
+         usage
+         ;;
+      \?)
+         usage
+         ;;
+   esac
+done
+
+file_path="$1"
+key="$2"
+
+mv "$file_path" "${file_path}.origin"
+cat "${file_path}.origin" | sed -e 's/\t/\    /' | sed -e "/\"${key}\":/d" >"$file_path"
+rm "${file_path}.origin"
