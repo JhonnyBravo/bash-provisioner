26eb5b6a76b181c693f8b2046540e6266c438821
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:02:04 +0900

    modified: ps/remove_item.sh
    インデントサイズを修正。

--- ps/remove_item.sh
+++ ps/remove_item.sh
@@ -3,16 +3,16 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   remove_item.sh [-r] path [-h]
+  remove_item.sh [-r] path [-h]
 
 Description:
-   ファイル / ディレクトリを削除します。
-   ディレクトリが空ではない場合、ディレクトリの削除は失敗します。
+  ファイル / ディレクトリを削除します。
+  ディレクトリが空ではない場合、ディレクトリの削除は失敗します。
 
 Options:
-   -r ディレクトリを再帰的に削除します。ディレクトリが空ではない場合、
-      残っているファイル / ディレクトリも含めて削除します。
-   -h ヘルプを表示します。
+  -r ディレクトリを再帰的に削除します。ディレクトリが空ではない場合、
+     残っているファイル / ディレクトリも含めて削除します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
@@ -21,27 +21,27 @@ r_flag=0
 
 while getopts "r:h" option
 do
-   case $option in
-      r)
-         r_flag=1
-         ;;
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    r)
+      r_flag=1
+      ;;
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 path="$1"
 
 if [ -f "$path" ]; then
-   rm "$path"
+  rm "$path"
 elif [ -d "$path" ]; then
-   rmdir "$path"
+  rmdir "$path"
 elif [ $r_flag -eq 1 ]; then
-   shift $(expr $OPTIND - 2)
-   path="$1"
-   rm -R "$path"
+  shift $(expr $OPTIND - 2)
+  path="$1"
+  rm -R "$path"
 fi
