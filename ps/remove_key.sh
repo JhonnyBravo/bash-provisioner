c0da625c1f22440b346836db409f2f3742e25125
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:02:37 +0900

    modified: ps/remove_key.sh
    インデントサイズを修正。

--- ps/remove_key.sh
+++ ps/remove_key.sh
@@ -3,27 +3,27 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   remove_key.sh file_path key [-h]
+  remove_key.sh file_path key [-h]
 
 Description:
-   JSON ファイルから key を削除します。
+  JSON ファイルから key を削除します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 file_path="$1"
