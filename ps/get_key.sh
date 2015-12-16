f0c3712bd8bb6f557be1fc881d4d89e19e163be4
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 21:53:55 +0900

    modified: ps/get_key.sh
    インデントサイズを修正。

--- ps/get_key.sh
+++ ps/get_key.sh
@@ -3,27 +3,27 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   get_key.sh file_path key [-h]
+  get_key.sh file_path key [-h]
 
 Description:
-   JSON ファイルの key の値を取得します。
+  JSON ファイルの key の値を取得します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 file_path="$1"
