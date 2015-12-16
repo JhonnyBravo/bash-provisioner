a77833b368a90a32a206f8e8ad4e87f8d86f4313
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:07:13 +0900

    modified: ps/test_path.sh
    インデントサイズを修正。

--- ps/test_path.sh
+++ ps/test_path.sh
@@ -3,18 +3,18 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   test_path.sh [-l] [-c] path [-h]
+  test_path.sh [-l] [-c] path [-h]
 
 Description:
-   path が実在すれば 0 を、存在しない場合は 1 を返します。
-   オプションを省略した場合、 
-   path がファイルであるかディレクトリであるかを区別せず、
-   実在する path であれば 0 を返します。
+  path が実在すれば 0 を、存在しない場合は 1 を返します。
+  オプションを省略した場合、 
+  path がファイルであるかディレクトリであるかを区別せず、
+  実在する path であれば 0 を返します。
 
 Options:
-   -l path path が実在し、かつファイルであれば 0 を返します。
-   -c path path が実在し、かつディレクトリであれば 0 を返します。
-   -h ヘルプを表示します。
+  -l path path が実在し、かつファイルであれば 0 を返します。
+  -c path path が実在し、かつディレクトリであれば 0 を返します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
@@ -24,31 +24,31 @@ c_flag=0
 
 while getopts "lch" option
 do
-   case $option in
-      l)
-         l_flag=1
-         ;;
-      c)
-         c_flag=1
-         ;;
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    l)
+      l_flag=1
+      ;;
+    c)
+      c_flag=1
+      ;;
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 if [ $l_flag -eq 1 ]; then
-   shift $(expr $OPTIND - 1)
-   path="$1"
-   test -f "${path}"
+  shift $(expr $OPTIND - 1)
+  path="$1"
+  test -f "${path}"
 elif [ $c_flag -eq 1 ]; then
-   shift $(expr $OPTIND - 1)
-   path="$1"
-   test -d "${path}"
+  shift $(expr $OPTIND - 1)
+  path="$1"
+  test -d "${path}"
 elif [ $l_flag -eq 0 -a $c_flag -eq 0 ]; then
-   path="$1"
-   test -e "${path}"
+  path="$1"
+  test -e "${path}"
 fi
