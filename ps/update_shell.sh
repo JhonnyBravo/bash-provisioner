b1189e7d3f45216793dfa00cef0d3bcc285415ea
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:07:54 +0900

    modified: ps/update_shell.sh
    インデントサイズを修正。

--- ps/update_shell.sh
+++ ps/update_shell.sh
@@ -2,25 +2,25 @@
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         cat <<_EOT_
+  case $option in
+    h)
+      cat <<_EOT_
 Usage:
-   update_shell.sh [-h]
+  update_shell.sh [-h]
 
 Description:
-   dash を有効化 / 無効化します。
+  dash を有効化 / 無効化します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
-         exit 1
-         ;;
-      \?)
-         echo "-h オプションで使用法を確認してください。"
-         exit 1
-         ;;
-   esac
+      exit 1
+      ;;
+    \?)
+      echo "-h オプションで使用法を確認してください。"
+      exit 1
+      ;;
+  esac
 done
 
 dpkg-reconfigure dash
