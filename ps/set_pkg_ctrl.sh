8f2494a36f71f989caf9c4030252d0ff40a2de80
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:05:19 +0900

    modified: ps/set_pkg_ctrl.sh
    インデントサイズを修正。

--- ps/set_pkg_ctrl.sh
+++ ps/set_pkg_ctrl.sh
@@ -5,27 +5,27 @@ u_path=".config/sublime-text-2/Packages/User/"
 function usage(){
 cat <<_EOT_
 Usage:
-   set_pkg_ctrl.sh path [-h]
+  set_pkg_ctrl.sh path [-h]
 
 Description:
-   指定したファイルを User/Package Control.sublime-settings へコピーします。
+  指定したファイルを User/Package Control.sublime-settings へコピーします。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 cp "$1" ~/"${u_path}"
