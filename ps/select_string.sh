982e61bf08124e9aee722b32da62dd793918d148
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-17 05:33:57 +0900

    modified: ps/select_string.sh
    shellcheck に従ってコードを修正。
    ヘルプテキストを修正。

--- ps/select_string.sh
+++ ps/select_string.sh
@@ -9,11 +9,11 @@ Usage:
 
 Description:
   path に指定したファイルを読み込み、
-  pattern に合致する行 / 合致しない行のみを表示します。
+  pattern に合致する行または合致しない行のみを表示します。
 
 Options:
-  -i pattern path pattern に合致する行のみを表示します。
-  -e pattern path pattern に合致しない行のみを表示します。
+  -i pattern path  pattern に合致する行のみを表示します。
+  -e pattern path  pattern に合致しない行のみを表示します。
   -h ヘルプを表示します。
 _EOT_
 exit 1
@@ -40,14 +40,14 @@ do
   esac
 done
 
-shift $(expr $OPTIND - 1)
+shift $((OPTIND - 1))
 pattern="$1"
 path="$2"
 
 if [ $i_flag -eq 1 ]; then
-  cat "${path}" | grep "${pattern}"
+  grep "${pattern}" <"$path"
 elif [ $e_flag -eq 1 ]; then
-  cat "${path}" | sed -e "/${pattern}/d"
+  sed -e "/${pattern}/d" <"$path"
 elif [ $i_flag -eq 0 -a $e_flag -eq 0 ]; then
   usage
 fi
