dba4408a0d0318ddc07c785a1ebc7ae72f49e331
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 21:57:15 +0900

    modified: ps/provision_git.sh
    インデントサイズを修正。

--- ps/provision_git.sh
+++ ps/provision_git.sh
@@ -8,54 +8,54 @@ c_flag=0
 function usage(){
 cat <<_EOT_
 Usage:
-   provision_${1}.sh [-i] [-u] [-c] [-h]
+  provision_${1}.sh [-i] [-u] [-c] [-h]
 
 Description:
-   ${1} をインストール / アンインストールします。
-   または ${1} へユーザ情報を登録します。
+  ${1} をインストール / アンインストールします。
+  または ${1} へユーザ情報を登録します。
 
 Options:
-   -i ${1} をインストールします。
-   -u ${1} をアンインストールします。
-   -c ${1} へユーザ情報を登録します。
-   -h ヘルプを表示します。
+  -i ${1} をインストールします。
+  -u ${1} をアンインストールします。
+  -c ${1} へユーザ情報を登録します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "iuch" option
 do
-   case $option in
-      i)
-         i_flag=1
-         ;;
-      u)
-         u_flag=1
-         ;;
-      c)
-         c_flag=1
-         ;;
-      h)
-         usage "$package"
-         ;;
-      \?)
-         usage "$package"
-         ;;
-   esac
+  case $option in
+    i)
+      i_flag=1
+      ;;
+    u)
+      u_flag=1
+      ;;
+    c)
+      c_flag=1
+      ;;
+    h)
+      usage "$package"
+      ;;
+    \?)
+      usage "$package"
+      ;;
+  esac
 done
 
 if [ $i_flag -eq 1 ]; then
-   apt-get install $package
+  apt-get install $package
 elif [ $u_flag -eq 1 ]; then
-   apt-get purge $package
+  apt-get purge $package
 elif [ $c_flag -eq 1 ]; then
-   echo -n "ユーザ名を入力してください: "
-   read name
+  echo -n "ユーザ名を入力してください: "
+  read name
 
-   echo -n "E mail アドレスを入力してください: "
-   read email
+  echo -n "E mail アドレスを入力してください: "
+  read email
 
-   git config --global color.ui auto
-   git config --global user.name "$name"
-   git config --global user.email "$email"
+  git config --global color.ui auto
+  git config --global user.name "$name"
+  git config --global user.email "$email"
 fi
