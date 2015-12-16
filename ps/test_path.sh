a72859be1f7d27f4a0988e5d02ee5c37c6d783ca
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-17 04:25:07 +0900

    modified: ps/test_path.sh
    shellcheck に従ってコードを修正。
    ヘルプテキストを修正。

--- ps/test_path.sh
+++ ps/test_path.sh
@@ -3,7 +3,8 @@
 function usage(){
 cat <<_EOT_
 Usage:
-  test_path.sh [-l] [-c] path [-h]
+  test_path.sh path
+  test_path.sh [-l path] [-c path] [-h]
 
 Description:
   path が実在すれば 0 を、存在しない場合は 1 を返します。
@@ -12,8 +13,8 @@ Description:
   実在する path であれば 0 を返します。
 
 Options:
-  -l path path が実在し、かつファイルであれば 0 を返します。
-  -c path path が実在し、かつディレクトリであれば 0 を返します。
+  -l path  path が実在し、かつファイルであれば 0 を返します。
+  -c path  path が実在し、かつディレクトリであれば 0 を返します。
   -h ヘルプを表示します。
 _EOT_
 exit 1
@@ -41,11 +42,11 @@ do
 done
 
 if [ $l_flag -eq 1 ]; then
-  shift $(expr $OPTIND - 1)
+  shift $((OPTIND - 1))
   path="$1"
   test -f "${path}"
 elif [ $c_flag -eq 1 ]; then
-  shift $(expr $OPTIND - 1)
+  shift $((OPTIND - 1))
   path="$1"
   test -d "${path}"
 elif [ $l_flag -eq 0 -a $c_flag -eq 0 ]; then
