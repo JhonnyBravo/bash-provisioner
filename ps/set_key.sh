7b9baaba3a418b2380bbd43ebd502afb87585998
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:04:52 +0900

    modified: ps/set_key.sh
    インデントサイズを修正。

--- ps/set_key.sh
+++ ps/set_key.sh
@@ -3,27 +3,27 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   set_key.sh file_path key:value [-h]
+  set_key.sh file_path key:value [-h]
 
 Description:
-   JSON ファイルの key の値を更新します。
+  JSON ファイルの key の値を更新します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 file_path="$1"
