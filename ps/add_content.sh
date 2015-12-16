7b054b132a661a7a43ec6f8265b9f29d6779c49b
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 21:50:57 +0900

    modified: ps/add_content.sh
    インデントサイズを修正。

--- ps/add_content.sh
+++ ps/add_content.sh
@@ -3,27 +3,27 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   add_content.sh path value [-h]
+  add_content.sh path value [-h]
 
 Description:
-   ファイルへ値を追記します。
+  ファイルへ値を追記します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 path="$1"
