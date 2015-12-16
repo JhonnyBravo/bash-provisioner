101ea031a965aaf5612f52cbdc953fb0699a1d27
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 21:52:55 +0900

    modified: ps/enable_vboxsf.sh
    インデントサイズを修正。

--- ps/enable_vboxsf.sh
+++ ps/enable_vboxsf.sh
@@ -3,27 +3,27 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   enable_vboxsf.sh [-h]
+  enable_vboxsf.sh [-h]
 
 Description:
-   Virtual Box のゲスト OS へ共有ディレクトリへのアクセス権を付与します。
+  Virtual Box のゲスト OS へ共有ディレクトリへのアクセス権を付与します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 user_name="$1"
