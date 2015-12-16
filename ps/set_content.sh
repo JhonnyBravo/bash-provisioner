7c4dc990270ee60b792a51fd8877916a8c9e564a
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 22:04:24 +0900

    modified: ps/set_content.sh
    インデントサイズを修正。

--- ps/set_content.sh
+++ ps/set_content.sh
@@ -3,28 +3,28 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   set_content.sh path value [-h]
+  set_content.sh path value [-h]
 
 Description:
-   ファイルへ値を書き込みます。
-   ファイルの内容が空ではない場合、既存の内容を削除して上書きします。
+  ファイルへ値を書き込みます。
+  ファイルの内容が空ではない場合、既存の内容を削除して上書きします。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 path="$1"
