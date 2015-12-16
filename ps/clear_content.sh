7bc91ca30ac727432b7b831356f4de9c57075ba4
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-16 21:52:16 +0900

    modified: ps/clear_content.sh
    インデントサイズを修正。

--- ps/clear_content.sh
+++ ps/clear_content.sh
@@ -3,27 +3,27 @@
 function usage(){
 cat <<_EOT_
 Usage:
-   clear_content.sh path [-h]
+  clear_content.sh path [-h]
 
 Description:
-   ファイルの内容を削除します。
+  ファイルの内容を削除します。
 
 Options:
-   -h ヘルプを表示します。
+  -h ヘルプを表示します。
 _EOT_
 exit 1
 }
 
 while getopts "h" option
 do
-   case $option in
-      h)
-         usage
-         ;;
-      \?)
-         usage
-         ;;
-   esac
+  case $option in
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
 done
 
 path="$1"
