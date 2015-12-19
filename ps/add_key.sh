5d38504b68041c29bc5eb36a696b36fdd7276db6
Jhonny Bravo <sanfranceshika5@gmail.com>
2015-12-20 00:50:09 +0900

    modified: ps/add_key.sh
    ヘルプテキストを修正。
    sed の正規表現を修正。

--- ps/add_key.sh
+++ ps/add_key.sh
@@ -3,7 +3,7 @@
 function usage(){
 cat <<_EOT_
 Usage:
-  add_key.sh file_path key:value [-h]
+  ${0} path key:value [-h]
 
 Description:
   JSON ファイルへ新しい key:value を追加します。
@@ -26,9 +26,9 @@ do
   esac
 done
 
-file_path="$1"
+path="$1"
 key="$2"
 
-mv "$file_path" "${file_path}.origin"
-sed -e 's/\t/\    /' <"${file_path}.origin" | sed -e "/^}/i \    ${key}" >"$file_path"
-rm "${file_path}.origin"
+mv "$path" "${path}.origin"
+sed -e 's/\t/\  /g' <"${path}.origin" | sed -e "/^}/i \  ${key}" >"$path"
+rm "${path}.origin"
