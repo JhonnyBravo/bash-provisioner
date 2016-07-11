1335e03bac364915b6573af609a8d8afa1e7efd9
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-07-11 19:07:08 +0000

    modified: bin/set_content.sh
    modified function: usage

--- bin/set_content.sh
+++ bin/set_content.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+script_name=$(basename "$0")
+
 function usage(){
 cat <<_EOT_
-Usage:
-  ${0} path value [-h]
+NAME
+       ${script_name}
 
-Description:
-  ファイルへ値を書き込みます。
-  ファイルの内容が空ではない場合、既存の内容を削除して上書きします。
+USAGE:
+       ${script_name} path value [-h]
 
-Options:
-  -h ヘルプを表示します。
+
+DESCRIPTION:
+       ファイルへ値を書き込みます。
+       ファイルの内容が空ではない場合、既存の内容を削除して上書きします。
+
+ARGUMENTS:
+       path   値を書き込むファイルのパス。
+
+       value  ファイルへ書き込む値。
+
+OPTIONS:
+       -h     ヘルプを表示します。
 _EOT_
 exit 1
 }
