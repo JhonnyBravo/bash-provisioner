ac73deace4b0b1deb42388f727ed4b965e9480c6
Jhonny Bravo <sanfranceshika5@gmail.com>
2016-11-27 02:08:34 +0900

    modified: setup.sh
    マニュアルページのインストール / アンインストール処理を削除。
    ヘルプメッセージを修正。

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@
 script_source="bin"
 script_destination="/usr/local/bin"
 
-man_source="docs/build/man"
-man_destination="/usr/local/man/man1"
-
 i_flag=0
 u_flag=0
 
@@ -15,19 +12,15 @@ Usage:
   ${0} [-i] [-u] [-h]
 
 Description:
-  スクリプトファイルとマニュアルページを
-  インストール / アンインストールします。
+  スクリプトファイルをインストール / アンインストールします。
 
 Files:
   ${script_destination}: スクリプトファイルのインストール先。
   ${script_source}: インストールするスクリプトを収めたディレクトリ。
 
-  ${man_destination}: マニュアルページのインストール先。
-  ${man_source}: インストールするマニュアルを収めたディレクトリ。
-
 Options:
-  -i スクリプトファイルとマニュアルページをインストールします。
-  -u スクリプトファイルとマニュアルページをアンインストールします。
+  -i スクリプトファイルをインストールします。
+  -u スクリプトファイルをアンインストールします。
   -h ヘルプを表示します。
 _EOT_
 exit 1
@@ -63,45 +56,10 @@ if [ $i_flag -eq 1 ]; then
     install "$source" "$script_destination"
   done
 
-  # マニュアルページのインストール
-  which sphinx-build
-
-  if [ $? -ne 0 ]; then
-    provision_python_tools.sh -i
-  fi
-
-  (
-    cd docs || exit 2
-    make clean man
-  )
-
-  test_path.sh -c "$man_destination"
-
-  if [ $? -ne 0 ]; then
-    new_item.sh -d "$man_destination"
-  fi
-
-  man_list=$(ls "$man_source")
-
-  for file in $man_list
-  do
-    source="${man_source}/${file}"
-    gzip "$source"
-    install "${source}.gz" "$man_destination" -m 0644
-  done
 elif [ $u_flag -eq 1 ]; then
   # スクリプトファイルのアンインストール
   for file in $script_list
   do
     rm "${script_destination}/${file}"
-  done
-
-  # マニュアルページのアンインストール
-  man_list=$(ls "$man_source")
-
-  for file in $man_list
-  do
-    source="${man_destination}/${file}"
-    rm "${source}"
   done
 fi
